0239a9b050444609e24cb5b9d709c454d2e93919
mznco <admin@mznco.net>
2017-07-19 14:58:58 -0400

    renamed project

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='spdb_wrapper',
-	  version='0.3',
-	  description='An extremely simple interface for sailplay databases. ',
-	  url='https://github.com/mznco/spdb_wrapper',
-	  author='mznco',
-	  author_email='admin@mznco.net',
-	  license='GPL-3.0',
-	  packages=['spdb_wrapper'],
-	  install_requires=[
-	  ],
-	  zip_safe=False)
+setup(name='mzdb_wrapper',
+    version='0.3',
+    description='An extremely simple interface for databases. ',
+    url='https://github.com/mznco/mzdb_wrapper',
+    author='mznco',
+    author_email='admin@mznco.net',
+    license='GPL-3.0',
+    packages=['mzdb_wrapper'],
+    install_requires=[
+    ],
+    zip_safe=False)
