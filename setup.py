fab79279ae8eea3d0e7117a2064d49af4121d573
mznco <admin@mznco.net>
2017-07-21 11:16:16 -0400

    bumped version #

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mzdb_wrapper',
-    version='0.3',
+    version='0.4',
     description='An extremely simple interface for databases. ',
     url='https://github.com/mznco/mzdb_wrapper',
     author='mznco',
